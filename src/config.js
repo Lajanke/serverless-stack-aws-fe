0b2ca19dec262dd2c2563841583fd7ea1aefd2b8
Laura Kenny <l.j.kenny@hotmail.com>
2020-09-05 16:51:47 +0100

    create new note component.

--- src/config.js
+++ src/config.js
@@ -12,5 +12,6 @@ export default {
     USER_POOL_ID: "us-east-2_fafUdms16",
     APP_CLIENT_ID: "4ruo4o1id8o6tkjcr9h3tqknlu",
     IDENTITY_POOL_ID: "us-east-2:a59d0af4-de89-4eac-8a54-7257b87e378a"
-  }
+  },
+  MAX_ATTACHMENT_SIZE: 5000000,
 };
